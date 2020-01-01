cca769fe67df911051a6607a783919dbe3feb1ed
Rudresh Sisode <rudranrajput@gmail.com>
2020-01-01 09:18:39 +0530

    Testing

--- Unit-Testing/test/testcase.js
+++ Unit-Testing/test/testcase.js
@@ -1,6 +1,9 @@
 var math=require('../math')
 var user=require('../user')
-var expect=require('chai').expect;//npm install chai --save-dev
+var expect=require('chai').expect;//npm install chai --save-dev.
+//expect is interface
+
+
 /*
 if(math.sum(10,5) == 15){
     console.log("success")
@@ -8,6 +11,25 @@ if(math.sum(10,5) == 15){
 else{
     console.log("fail")
 }*/
+/*
+descrive 
+it 
+beforeEach
+please start from 52
+*/
+
+beforeEach(function(){
+
+    console.log("running beforeEach()");
+})
+
+beforeEach(function(){
+
+
+
+    console.log("after afterEach");
+})
+
 describe('mathematic test cases',function(){
     it('sum of two number',function(){
         expect(math.sum(10,5)).equal(15);
